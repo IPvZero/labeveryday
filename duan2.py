fcf8cd148fb93ba1f58dbb2954945ac5d3329344
IPvZero <46572101+IPvZero@users.noreply.github.com>
2020-11-28 03:24:11 +0000

    Create duan2.py

---
+++ duan2.py
@@ -0,0 +1,8 @@
+from nornir import InitNornir
+from nornir_netmiko.tasks import netmiko_send_command
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+results = nr.run(task=netmiko_send_command, command_string="show version")
+print_result(results)
